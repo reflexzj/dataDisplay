1a2c2bcf41a63f2bba684cad0dab3d073ce54b08
reflexzj <813577079@qq.com>
2018-01-11 14:16:08 +0800

    区镇入口

---
+++ dataDisplay/flaskapp/sums_models/area_sel/area_data.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+from dataDisplay.flaskapp.sums_models.methods import *
+
+def get_area_data(table_name, area_name):
+    '''
+    调用，返回对应区镇的数据
+    :param table_name:
+    :param area_name:
+    :return:
+    '''
+    data_by_area(table_name, area_name)
